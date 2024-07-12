d2b8bc2846b70d5ee2032d120c03bb79f3f6a1ce
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-07-12 12:44:39 -0400

    remove unneccessary variables

--- compute.tf
+++ compute.tf
@@ -2,9 +2,7 @@ provider "oci" {
     tenancy_ocid = var.tenancy_ocid
     user_ocid = var.user_ocid
     fingerprint = var.fingerprint
-    private_key_path = var.private_key_path
     region = var.region
-    private_key_password = var.private_key_password
 }
 
 variable "availability_domain" {
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
 variable "compartment_id" {}
 variable "home_region" {}
 variable "region" {}
